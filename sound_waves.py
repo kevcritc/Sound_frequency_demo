7c290b90132c3fb2649e2c0387dc255679dce147
Kevin Critchley <k.critchley@leeds.ac.uk>
2025-06-13 14:36:47 +0100

    additions

--- sound_waves.py
+++ sound_waves.py
@@ -12,8 +12,47 @@ import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 import tkinter as Tk
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,  NavigationToolbar2Tk)
+import matplotlib.animation as animation
 import threading
 from queue import Queue
+import matplotlib.patches as patches
+from math import pi
+
+def plot_waves(size_fig=3):
+    maxnumber=4
+    fig, ax=plt.subplots(maxnumber, figsize=(size_fig,3))
+    xdata=np.linspace(0, pi, num=200)
+    
+    for w in range(1,maxnumber+1):
+        ax[maxnumber-w].plot(xdata, np.sin(xdata*w), color='black' )
+        ax[maxnumber-w].plot(xdata, -1*np.sin(xdata*w), color='black' )
+        ax[maxnumber-w].plot(xdata, [0]*len(xdata), '--', color='black' )
+        ax[maxnumber-w].set_xlim(0, pi)
+        ax[maxnumber-w].axis('off')
+        ax[maxnumber-w].text(0,0.5, f'n={w}')
+        
+    fig.patches.append(
+        patches.Rectangle(
+            (0, 0.0),  # Lower-left corner in figure coords (0 to 1)
+            0.125, 1,    # Width and height in figure coords
+            transform=fig.transFigure,
+            facecolor='black',
+            alpha=0.2,
+            edgecolor='black'
+        )
+    )
+    fig.patches.append(
+        patches.Rectangle(
+            (0.9, 0),  # Lower-left corner in figure coords (0 to 1)
+            0.125, 1,    # Width and height in figure coords
+            transform=fig.transFigure,
+            facecolor='black',
+            alpha=0.2,
+            edgecolor='black'
+        )
+    )
+    
+    return fig, ax
 
 
 def record_and_plot(root,print_queue):    
@@ -89,6 +128,22 @@ def record_and_plot(root,print_queue):
         sd.play(sine_wave, FS)
         sd.wait()
 
+def plotwaves():
+    sizes=[3, 9]
+    for n in sizes:
+        fig, ax=plot_waves(size_fig=n)
+        window = Tk.Toplevel(root)
+        window.title(f'Length = {n}')
+    
+        canvas1 = FigureCanvasTkAgg(fig, master=window)
+        canvas1.draw()
+        canvas1.get_tk_widget().pack()
+    
+        toolbar = NavigationToolbar2Tk(canvas1, window)
+        toolbar.update()
+        plt.close(fig)
+    
+
 
 def dialogue_queue_worker(stop_event, dialogbox, print_queue):
     while not stop_event.is_set():
@@ -112,7 +167,7 @@ print_queue=Queue()
 stop_event = threading.Event()
 root = Tk.Tk()
 root.title('Dominant Frequency Analyser - Critchley')
-dialogbox = Tk.Text(root, height=15, width=50, font=10, wrap=Tk.WORD)
+dialogbox = Tk.Text(root, height=5, width=30, font=10, wrap=Tk.WORD)
 dialogbox.pack()
 workerthread = threading.Thread(
     target=dialogue_queue_worker,          
@@ -122,5 +177,7 @@ workerthread = threading.Thread(
 
 button_record=Tk.Button(root, text='Record Sound', command=lambda:run_soundthread(root, print_queue))
 button_record.pack()
+button_waves=Tk.Button(root, text='Create Diagram', command=plotwaves)
+button_waves.pack()
 workerthread.start()
 root.mainloop()
